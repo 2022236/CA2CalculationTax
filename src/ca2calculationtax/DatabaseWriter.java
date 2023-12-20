3aababa2d901c2175773c26deabbc148648de503
2022236 <146198723+2022236@users.noreply.github.com>
2023-12-20 14:06:50 +0000

    Update DatabaseWriter.java
    Updating the arguments of the methods where is matching more with the table. However, still not finished.

--- src/ca2calculationtax/DatabaseWriter.java
+++ src/ca2calculationtax/DatabaseWriter.java
@@ -11,19 +11,20 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- *
- * @author lizam
+ * Updating the method addUserDatabse since it is more accurate to the table, matching values and in the DBWriter 
+ * where is responsible for writing new information on the actual  Database
+ * @author 2022236 and 2022404
  */
-public class DatabaseWriter extends Database{
-    public boolean addUserDatabase(String userName, String role, String email, String password) throws SQLException {
-        IOUtils scan = new IOUtils ();
-        try (
+public class DatabaseWriter extends Database{ // Now arguments are better and matching with the attributes the table has
+   public boolean addUserDatabase(String userName ,String FirstName, String LastName,String Position, Date BirthDate, String Email, String PassWord, commonVariables.Type Type, commonVariables.MaritalStatus MaritalStatus) throws SQLException {
+        IOUtils scan = new IOUtils (); 
+        try ( // trycatch being a good option to try the code and if there is an error, catch will catch and return false
             Connection conn = DriverManager.getConnection(db_URL, USER, PASSWORD);
-            Statement stmt = conn.createStatement();  
-        ) {
-            String sql = String.format("INSERT INTO " + tableName + " (userName, role, email, password) VALUES ('%s', '%s', '%s', '%s');",
-     userName, role, email, password);
-
+            Statement stmt = conn.createStatement();    
+        ) { //sql and java command to create a table 
+            String sql = String.format("INSERT INTO " + tableName + " (userName, FirstName, LastName,Position, BirthDate, Email, PassWord, Type, MaritalStatus) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');",
+        userName,FirstName, LastName,Position, BirthDate, Email, PassWord, Type, MaritalStatus);
+            
             stmt.execute(sql);
             return true;
         } catch (Exception e) {
